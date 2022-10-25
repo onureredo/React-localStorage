948f7156ea31ba60cc7a8b6308999460f4019f13
Onur Eredo <onureredo@yahoo.com>
2022-10-25 09:16:13 +0200

    edit app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Local from './components/Local';
 // import SessionStorage from './components/SessionStorage';
 
+
 function App() {
   return (
     <div className="App">
+
       <h1>LOCAL STORAGE</h1>
       <h2>CHECK YOUR CONSOLE</h2>
+      
       <Local />       
     </div>
   );
